54f10856bb1d994202d93b0fab2ac93178624ff4
Sivateja <ajjarapusivateja123@gmail.com>
2025-07-02 16:08:05 +0530

    Add linkedlist problems

--- linkedlist/PartitionList.java
+++ linkedlist/PartitionList.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class PartitionList {
+public class PartitionList {
+    //LeetCode - 86
+    public ListNode partition(ListNode head, int x) {
+        //maintain two dummy heads
+        ListNode smaller =new ListNode(-1);
+        ListNode higher = new ListNode(-1);
+        ListNode smallerHead = smaller;
+        ListNode higherHead = higher;
+
+        while(head!=null){
+            //compare values of original list and link to smalle ror higher lists
+            if(head.val<x){
+                smallerHead.next=head;
+                smallerHead=smallerHead.next;
+            }else{
+                higherHead.next=head;
+                higherHead=higherHead.next;
+            }
+            head=head.next;
+
+
+        }
+        //avoid cycle by cutting off the last node in the 'higher' list
+        higherHead.next=null;
+        //Link smaller and higher lists
+        smallerHead.next=higher.next;
+        return smaller.next;
+    }
 }
--- linkedlist/SwapPairs.java
+++ linkedlist/SwapPairs.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class SwapPairs {
+public class SwapPairs {
+    public ListNode swapPairs(ListNode head) {
+        //create a dummy Node
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode point = dummy;
+
+
+        while (point.next != null && point.next.next != null) {
+            ListNode swap1 = point.next;
+            ListNode swap2 = point.next.next;
+
+            //swap the two nodes
+
+            swap1.next = swap2.next;
+            swap2.next = swap1;
+
+            //Link point to second swap and prepare for next iteration
+
+            point.next = swap2;
+            point = swap1;
+        }
+        return dummy.next;
+
+    }
 }
